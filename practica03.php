873e1ace4650847a1cd0655b3c24f083459fbb37
Evelyn Benitez <evelyn.benitez.ponce@gmail.com>
2023-01-23 10:28:35 -0600

    First commit

---
+++ practica03.php
@@ -0,0 +1,10 @@
+
+<?php
+echo"<H1><center>FRASES CELEBRES</center></H1>";
+echo"<H2>1ºSocrates:Yo solo se que no se nada</H2>";
+echo"<H2>2ºSteve Jobs:Las cosas n tienen que cambar para que el mundo sea importante</H2>";
+echo"<H2>3ºAlber Einsten:Si buscas resultados distntos no hagas siempre lo mismo</H2>";
+echo"<H2>4ºMario Mendoza:La vida no es mas que un sufrimiento detras de otro</H2>";
+echo"<H2>5ºTomas Edison:El valor de una idea radica en el uso de la misma</H2>";
+echo"<H3><center>Jose Alberto Contreras Muños 5AMPR</center></H3>"
+?>
